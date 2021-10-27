42b772ac739c3c0a26e0080504bea92b80f38d73
ashok0425 <ashokmehta123y@gmail.com>
2021-10-27 08:09:13 +0545

    system footer updated

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -2,108 +2,104 @@
     <section class="custom-b-bot text-white custom-bg-blue">
         <div class="container">
             <div class="row">
-                <div class="col-8 pt-5 pb-3">
+                <div class="col-12 pt-5 pb-3">
                     <div class="row ">
-                        <div class="col-lg-3 col-6">
-                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">About</h5>
+                        
+                        <div class="col-lg-2 col-6">
+                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">Links</h5>
                             <ul class="custom-fs-12 footer-ul">
-                                <li class="mb-2"><a href="#" class="text-white">Contact Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">About Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Careers</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Stories</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Press</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Wholesale</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Corporate Information</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">About us</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">Term & Condition</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">Privacy Policy</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">Refund Policy</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">Price & Payment</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">Carrer</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">Download business plan</a></li>
+
+
+                   
                             </ul>
                         </div>
-                        <div class="col-lg-3 col-6">
-                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">HELP</h5>
+                        <div class="col-lg-2 col-6">
+                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">Acount</h5>
                             <ul class="custom-fs-12 footer-ul">
-                                <li class="mb-2"><a href="#" class="text-white">Contact Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">About Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Careers</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Stories</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Press</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Wholesale</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Corporate Information</a></li>
+                                <li class="mb-2"><a href="#" class="text-white">
+                                 Member Login
+                                </a></li>
+
+                                <li class="mb-2"><a href="#" class="text-white">Super Distributor Login</a></li>
+
+                                <li class="mb-2"><a href="#" class="text-white"> Distributor Login</a></li>
+
+                                <li class="mb-2"><a href="#" class="text-white">Seller Login</a></li>
+                            
                             </ul>
                         </div>
-                        <div class="col-lg-3 col-6">
-                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">POLICY</h5>
+                        @php
+                            $social=DB::connection('mysql2')->table('websites')->first();
+                        @endphp
+                        <div class="col-lg-2 col-6">
+                            <h5 class="custom-text-dark mb-3 custom-fs-14 text-uppercase">SOCIAL</h5>
                             <ul class="custom-fs-12 footer-ul">
-                                <li class="mb-2"><a href="#" class="text-white">Contact Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">About Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Careers</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Stories</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Press</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Wholesale</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Corporate Information</a></li>
+                                @if ($social->facebook)                                    
+                                <li class="mb-2"><a href="#" class="text-white"><i class="fab fa-facebook"></i> Facebook</a></li>
+                                @endif
+
+                                @if ($social->twitter)                                    
+                                <li class="mb-2"><a href="#" class="text-white"><i class="fab fa-twitter"></i> Twitter</a></li>
+                                @endif
+
+                                @if ($social->instagram)                                    
+                                <li class="mb-2"><a href="#" class="text-white"><i class="fab fa-instagram"></i>Instagram</a></li>
+                                @endif
+
+                                @if ($social->tiktok)                                    
+                                <li class="mb-2"><a href="#" class="text-white"><i class="fab fa-telegram"></i> Telegram</a></li>
+                                @endif
+
+                             
+                               
                             </ul>
                         </div>
-                        <div class="col-lg-3 col-6">
-                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">SOCIAL</h5>
-                            <ul class="custom-fs-12 footer-ul">
-                                <li class="mb-2"><a href="#" class="text-white">Contact Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">About Us</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Careers</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Stories</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Press</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">JAJBASHOP Wholesale</a></li>
-                                <li class="mb-2"><a href="#" class="text-white">Corporate Information</a></li>
-                            </ul>
-                        </div>
+
+
+
+                     
+
+                    <div class="col-lg-2 col-6">
+                        <h5 class="custom-text-dark mb-3 custom-fs-14 text-uppercase">Media</h5>
+                        <ul class="custom-fs-12 footer-ul">
+                            <li class="mb-2 d-flex custom-fs-14 align-items-center"><a href="#" class="text-white"> Image </a></li>
+                            <li class="mb-2 d-flex custom-fs-14 align-items-center"><a href="#" class="text-white">Video </a></li>
+
+                        </ul>
                     </div>
-                </div>
-                <div class="col-4  pt-5 pb-3">
-                    <div class="row  custom-b-left">
-                        <div class="col-lg-6 ">
-                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">Mail Us:</h5>
-                            <ul class="custom-fs-12 footer-ul">
-                                <li class="mb-0">JAJBASHOP Internet Private Limited,</li>
-                                <li class="mb-0">Buildings Alyssa, Begonia &</li>
-                                <li class="mb-0">Clove Embassy Tech Village,</li>
-                                <li class="mb-0">Outer Ring Road, Devarabeesanahalli Village,</li>
-                                <li class="mb-0">Bengaluru, 560103,</li>
-                                <li class="mb-0">Karnataka, India</li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6 ">
-                            <h5 class="custom-text-dark mb-3 custom-fs-12 text-uppercase">Registered Office Address:
-                            </h5>
-                            <ul class="custom-fs-12 footer-ul">
-                                <li class="mb-0">JAJBASHOP Internet Private Limited,</li>
-                                <li class="mb-0">Buildings Alyssa, Begonia &</li>
-                                <li class="mb-0">Clove Embassy Tech Village,</li>
-                                <li class="mb-0">Outer Ring Road, Devarabeesanahalli Village,</li>
-                                <li class="mb-0">Bengaluru, 560103,</li>
-                                <li class="mb-0">Telephone: <a href="tel:1800 202 9898">1800 202 9898</a> </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class=" custom-fs-14 custom-bg-blue">
-        <div class="container">
-            <div class="row pt-3">
-                <div class="col-sm-8">
-                    <ul class="d-flex justify-content-around text-white">
-                        <li class=""><img src="assets/img/footer-icon-1.svg" class="img-fluid me-2 mb-1" alt=""><a
-                                href="#" class="text-white">Sell On JAJBASHOP</a></li>
-                        <li class=""><img src="assets/img/footer-icon-2.svg" class="img-fluid me-2 mb-1" alt=""><a
-                                href="#" class="text-white">Help Center</a></li>
-                        <li class=""><img src="assets/img/footer-icon-3.svg" class="img-fluid me-2 mb-1" alt=""><a
-                                href="#" class="text-white">Advertise</a></li>
-                        <li class=""><img src="assets/img/footer-icon-4.svg" class="img-fluid me-2 mb-1" alt=""><a
-                                href="#" class="text-white">Gift Cards</a></li>
-                        <li class="">© 2007-2021 JAJBASHOP.com</li>
+                    @php
+                    $social=DB::connection('mysql2')->table('websites')->first();
+                @endphp
+                <div class="col-lg-2 col-6">
+                    <h5 class="custom-text-dark mb-3 custom-fs-14 text-uppercase">Contact ifno</h5>
+                    <ul class="custom-fs-12 footer-ul">
+                        <li class="mb-2 d-flex custom-fs-14 align-items-center"><i class="fas fa-envelope mr-2"></i><a href="#" class="text-white"> &nbsp; {{ $social->email}}</a></li>
+                        <li class="mb-2 d-flex custom-fs-14 align-items-center"><i class="fas fa-phone-alt mr-2"></i><a href="#" class="text-white">&nbsp; {{ $social->phone}}</a></li>
+
+                        <li class="mb-2 d-flex custom-fs-14 align-items-center"><i class="fas fa-map-marker-alt mr-2"></i><a href="#" class="text-white">&nbsp;&nbsp; {{ $social->address}}</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-4 ">
-                    <img src="assets/img/payment-method_69e7ec.svg" alt="" class="img-fluid">
+
+<div class="col-lg-2">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14302.438586353122!2d87.043413!3d26.3391367!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5343632e81073a08!2sJajbashop!5e0!3m2!1sen!2snp!4v1635300958556!5m2!1sen!2snp" width="100%" height="150" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+
+
+
+
+
+                    </div>
                 </div>
+                
             </div>
         </div>
     </section>
+   
 </div>
