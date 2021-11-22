a616606a4fa27802ff3d218d685fda33f912ae9e
ashok0425 <ashokmehta123y@gmail.com>
2021-11-22 08:13:29 +0545

    updated

--- app/Http/Controllers/Admin/Repurchase/RepurchaseController.php
+++ app/Http/Controllers/Admin/Repurchase/RepurchaseController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin\repurchase;
 use App\Http\Controllers\Controller;
-use App\Models\repurchasecommision;
+use App\Models\Repurchasecommision;
 use Illuminate\Http\Request;
 
 class repurchaseController extends Controller
@@ -10,7 +10,7 @@ class repurchaseController extends Controller
 
     public function index()
     {
-        $repurchase=repurchasecommision::all();
+        $repurchase=Repurchasecommision::all();
         return view('admin.repurchase.repurchase.index',compact('repurchase'));
     }
 
@@ -18,20 +18,20 @@ class repurchaseController extends Controller
  
     public function edit($id)
     {
-        $level=repurchasecommision::find($id);
+        $level=Repurchasecommision::find($id);
         return view('admin.repurchase.repurchase.edit',compact('level'));
     }
 
     public function update(Request $request)
     {
-        $level=repurchasecommision::find($request->id);
+        $level=Repurchasecommision::find($request->id);
         $level->min_bv=$request->min_bv;
         $level->max_bv=$request->max_bv;
         $level->percent=$request->percent;
 
         $level->save();
         $notification=array(
-            'messege'=>'repurchasecommision updated Sucessfully',
+            'messege'=>'Repurchasecommision updated Sucessfully',
              'alert-type'=>'success'
          );
 
