db23520c47d24fa659455915cdc9662027d63b15
ashok0425 <ashokmehta123y@gmail.com>
2022-01-13 16:22:12 +0545

    issue of logo in print and download page solve

--- resources/views/distributor/report/print.blade.php
+++ resources/views/distributor/report/print.blade.php
@@ -11811,8 +11811,9 @@ table {
                                     <div class="logo">
                                         <div class="img-wrap">
                                         <a href="https://jajbashop.in">
+        {{ __getimagePath($website->image) }}
 
-											<img src=" {{ __getimagePath($website->image) }}" alt="jajbashop.in" class="img-fluid">
+											<img src="{{ __getimagePath($website->image) }}" alt="jajbashop.in" class="img-fluid">
 										</a>
                                         </div>
                                     </div>
