ff8909de460cfbd59af0ae0c3eb272d45bbdbe8f
ashok0425 <ashokmehta123y@gmail.com>
2022-01-13 16:17:36 +0545

    issue of logo in print and download page solve

--- resources/views/distributor/report/show.blade.php
+++ resources/views/distributor/report/show.blade.php
@@ -137,6 +137,7 @@ if($order->seller==3){
     @foreach ($product as $item)
     <tr>
     <td>
+        {{ __getimagePath($item->image) }}
        <img src="{{ __getimagePath($item->image) }}" alt="Product image" class="img-fluid" width="80">
 
     </td>
